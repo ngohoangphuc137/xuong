88954865f88e9776f81bad50210e09f29aac2c5d
VUDINHCHINH <chinh1008204@gmail.com>
2024-01-21 17:12:03 +0700

    push

--- admin/index.php
+++ admin/index.php
@@ -1,18 +1,14 @@
- 
-
-
-
-
-
-   <?php
+ <?php
+    include './model/pdo.php';
+    include "../model/danhmuc.php";
 
     include "view/header.php";
     if (isset($_GET['act']) && $_GET['act'] != "") {
         $act = $_GET['act'];
        
+
     } else {
         include "view/home.php";
     }
-
-   
+    //hihi
     ?>
